79f2c87dfd7c01ae7227d4c0e2d0c0300f25aaca
maurbin <Diosmequiere0>
2021-03-14 14:11:43 +0100

    Task 2

--- app/src/main/java/com/example/android/materialme/MainActivity.java
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -19,6 +19,7 @@ package com.example.android.materialme;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -56,11 +57,21 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new SportsAdapter(this, mSportsData);
         mRecyclerView.setAdapter(mAdapter);
 
+        int gridColumnCount =
+                getResources().getInteger(R.integer.grid_column_count);
+        mRecyclerView.setLayoutManager(new
+                GridLayoutManager(this, gridColumnCount));
+        int swipeDirs;
+        if(gridColumnCount > 1){
+            swipeDirs = 0;
+        } else {
+            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        }
         // Get the data.
         initializeData();
         ItemTouchHelper helper = new ItemTouchHelper(new
                                                              ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-                                                                     ItemTouchHelper.DOWN | ItemTouchHelper.UP,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                                                                     ItemTouchHelper.DOWN | ItemTouchHelper.UP,swipeDirs) {
                                                                  @Override
                                                                  public boolean onMove(RecyclerView recyclerView,
                                                                                        RecyclerView.ViewHolder viewHolder,
@@ -80,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                                                                  }
                                                              });
         helper.attachToRecyclerView(mRecyclerView);
+
+
     }
 
     /**
